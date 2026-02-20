c4e7acc203bef3be4bffa1220ea96e5a389c1456
Wanderson Silva <wandersonsilva768@gmail.com>
2026-02-20 00:39:09 -0300

    feat: adiciona o verificador de PIX da V2

---
+++ src/pix-process-v2.ts
@@ -0,0 +1,372 @@
+
+import {
+  type PixOrderData,
+  type PixOrderDataPoll,
+  type ContraCorrentePixProcessData,
+} from '../types/pix-process-v2'
+
+import {
+  type FunctionSucceededPattern,
+  type Nullable,
+  type ResponsePattern,
+  type ResponsePatternCallback,
+} from '../types/global'
+
+import {
+  ref,
+  createApp,
+  defineComponent,
+} from 'vue'
+
+import {
+  NULL_VALUE,
+  isNull,
+  buildURL,
+  stringify,
+  getAttribute,
+  addAttribute,
+  isPageLoading,
+  querySelector,
+  safeParseJson,
+} from '../utils/dom'
+
+import {
+  EMPTY_STRING,
+  SLASH_STRING,
+  XANO_BASE_URL,
+} from '../utils/consts'
+
+import {
+  BRLFormatter,
+} from '../utils/mask'
+
+import {
+  postErrorResponse,
+  postSuccessResponse,
+  buildRequestOptions,
+} from '../utils/requestResponse'
+
+import {
+  HttpMethod,
+} from '../types/http'
+
+const DEFAULT_TIME = '00:00:00'
+
+const PROCESS_PAYMENT_URL = `${XANO_BASE_URL}/api:niMKesyM`
+
+const ORDER_CONFIRM_URL = '/order-confirmation'
+
+const orderParameter = 'order-id'
+
+async function logError (payload: object = {}): Promise<ResponsePattern<any>> {
+  const defaultErrorMessage = 'Falha ao enviar o log de erro'
+
+  try {
+    const response = await fetch(`${XANO_BASE_URL}/api:ltSOgTnO/beacon`, {
+      ...buildRequestOptions([], HttpMethod.POST),
+      body: stringify({
+        custom: payload,
+        environment: {
+          url: location.protocol.concat('//', location.hostname, location.pathname, location.search),
+        },
+      }),
+      keepalive: true,
+    })
+
+    if (!response.ok) {
+      const error = await response.json()
+
+      return postErrorResponse(error?.message)
+    }
+
+    const data = await response.json()
+
+    return postSuccessResponse(data)
+  } catch (e) {
+    return postErrorResponse(defaultErrorMessage)
+  }
+}
+
+const ContraCorrenteOrderPage = defineComponent({
+  name: 'PIXProcessPage',
+
+  setup () {
+    return {
+      hasEventSource: ref<Nullable<boolean>>(NULL_VALUE),
+    }
+  },
+
+  data (): ContraCorrentePixProcessData {
+    return {
+      now: Date.now(),
+      isLoading: true,
+      hasCopied: false,
+      order: NULL_VALUE,
+      nowInterval: NULL_VALUE,
+    }
+  },
+
+  /**
+   * Ações executadas logo após a criação do componente
+   */
+  async created (): Promise<void> {
+    this.hasEventSource = 'EventSource' in window
+
+    const searchParams = new URLSearchParams(location.search)
+
+    const transactionId = searchParams.get(orderParameter)
+
+    if (!transactionId) {
+      location.href = buildURL(SLASH_STRING, {
+        reason: 'no_transaction_id'
+      })
+
+      return
+    }
+
+    const response = await this.getOrder(transactionId)
+
+    if (!response.succeeded) {
+      logError({
+        ...response,
+        reason: 'request_response_failed',
+      })
+
+      location.href = buildURL(SLASH_STRING, {
+        reason: 'request_response_failed',
+      })
+
+      return
+    }
+
+    // if (response.data.payment_method !== 'pix') {
+    //   logError(response)
+    //
+    //   location.href = buildURL(SLASH_STRING, {
+    //     reason: 'wrong_payment_method',
+    //   })
+    //
+    //   return
+    // }
+
+    this.isLoading = false
+
+    this.order = response.data
+
+    this.$nextTick(() => this.setQRImage())
+
+    if (response.data.pago) {
+      setTimeout(() => {
+        location.href = buildURL(ORDER_CONFIRM_URL, {
+          [orderParameter]: transactionId
+        })
+      }, 5000)
+
+      return
+    }
+
+    if (response.data.expired) return
+
+    this.nowInterval = setInterval(() => this.now = Date.now(), 1000)
+
+    if (this.hasEventSource) {
+      this.pollOrder(transactionId)
+    }
+  },
+
+  methods: {
+    /**
+     * Inicia o SSE que ficará ouvindo modificações no pedido vindas do backend
+     */
+    pollOrder (orderId: string): void {
+      const source = new EventSource(`${PROCESS_PAYMENT_URL}/confirm-pix/${orderId}`)
+
+      source.addEventListener('message', async (event: MessageEvent<string>) => {
+        const orderData = safeParseJson<PixOrderDataPoll>(event.data)
+
+        if (isNull(orderData)) return
+
+        this.patchOrder({
+          ...orderData,
+          expired: DEFAULT_TIME === this.timmer || orderData.expired,
+        })
+
+        if (this.hasPaid || this.isExpired) {
+          source.readyState !== source.CLOSED && source.close()
+
+          this.clearInterval()
+        }
+
+        if (!this.hasPaid) return
+
+        setTimeout(() => {
+          location.href = buildURL(ORDER_CONFIRM_URL, {
+            [orderParameter]: orderId
+          })
+        }, 5000)
+      })
+
+      document.addEventListener('beforeunload', () => {
+        if (source.readyState === source.CLOSED) return
+
+        source.close()
+
+        this.clearInterval()
+      })
+    },
+
+    /**
+     * Atualiza as informações do pedido parcialmente
+     */
+    patchOrder ({ pago, expired, total }: PixOrderDataPoll): void {
+      if (!this.order) return
+
+      this.order.pago = pago
+      this.order.total = total
+      this.order.expired = expired
+    },
+
+    /**
+     * Captura inicial dos dados do pedido
+     */
+    async getOrder <T extends PixOrderData> (orderId: string): Promise<ResponsePattern<T>> {
+      const defaultErrorMessage = 'Falha ao capturar o pedido'
+
+      try {
+        const response = await fetch(`${PROCESS_PAYMENT_URL}/confirm-pix/${orderId}/rest`, {
+          ...buildRequestOptions(),
+          priority: 'high',
+        })
+
+        if (!response.ok) {
+          const error = await response.json()
+
+          return postErrorResponse.call(response, error?.message ?? defaultErrorMessage)
+        }
+
+        const data: T = await response.json()
+
+        return postSuccessResponse.call<Response, [T, ResponsePatternCallback?], FunctionSucceededPattern<T>>(response, data)
+      } catch (e) {
+        return postErrorResponse(defaultErrorMessage)
+      }
+    },
+
+    async handleCopyQRCode (): Promise<void> {
+      if (this.hasCopied) return
+
+      this.hasCopied = true
+
+      if (navigator.clipboard.writeText) {
+        await navigator.clipboard.writeText(this.order?.qrcode_text ?? '')
+      } else {
+        const input = document.createElement('input')
+
+        document.body.appendChild(input)
+
+        input.value = this.order?.qrcode_text ?? EMPTY_STRING
+
+        input.select()
+
+        document.execCommand('copy')
+
+        document.body.removeChild(input)
+      }
+
+      setTimeout(() => this.hasCopied = false, 3000)
+    },
+
+    setQRImage (): void {
+      const QRImage = querySelector('[data-wtf-qr-code-image]') as Nullable<HTMLImageElement>
+
+      if (!QRImage) return
+
+      QRImage.onload = () => isPageLoading(false)
+
+      addAttribute(
+        QRImage,
+        'src',
+        this.order?.qrcode ?? getAttribute(QRImage, 'src') ?? EMPTY_STRING
+      )
+    },
+
+    clearInterval (): void {
+      clearInterval(this.nowInterval as number)
+
+      this.nowInterval = NULL_VALUE
+    },
+  },
+
+  computed: {
+    orderPrice (): string {
+      const { order } = this
+
+      return BRLFormatter.format(
+        order
+          ? order.total / 100
+          : 0
+      )
+    },
+
+    timmer (): string {
+      if (!this.order || this.isExpired) return DEFAULT_TIME
+
+      const secondsDiff = Math.floor(Math.max((this.order?.due_time ?? 0) - this.now, 0) / 1000)
+
+      const hours = Math.floor(secondsDiff / 3600)
+      const minutes = Math.floor((secondsDiff % 3600) / 60)
+      const seconds = secondsDiff % 60
+
+      return [hours, minutes, seconds]
+        .map(time => time.toString().padStart(2, '0'))
+        .join(':')
+    },
+
+    /**
+     * Indica se o pedido consta pago
+     */
+    hasPaid (): boolean {
+      return this.order?.pago ?? false
+    },
+
+    /**
+     * Indica se o pedido consta expirado
+     */
+    isExpired (): boolean {
+      return this.order?.expired ?? false
+    },
+
+    /**
+     * Retorna a linha copiável do QRCode
+     */
+    getQRCode (): string {
+      return this.order?.qrcode_text ?? EMPTY_STRING
+    },
+  },
+
+  watch: {
+    timmer (time: string): void {
+      if (time !== DEFAULT_TIME) return
+
+      this.patchOrder({
+        expired: true,
+        total: this.order?.total ?? 0,
+        pago: this.order?.pago ?? false,
+      })
+    }
+  },
+})
+
+const pixProcessElement = querySelector('#pixProcess') as Nullable<globalThis.Element>
+
+if (!pixProcessElement) {
+  location.href = buildURL('/', {
+    reason: 'pix_element_not_found',
+  })
+} else {
+  createApp(ContraCorrenteOrderPage).mount(pixProcessElement)
+}
+
+window.addEventListener('pageshow', (e: PageTransitionEvent) => {
+  if (e.persisted) window.location.reload()
+})
---
+++ types/pix-process-v2.d.ts
@@ -0,0 +1,133 @@
+
+import {
+  type Nullable,
+  type ResponsePattern,
+  type ComputedReturnValues,
+} from './global'
+
+export interface ContraCorrentePixProcessSetup {
+  /**
+   * Indica se o navegador tem suporte ao EventSource
+   */
+  hasEventSource: Ref<Nullable<boolean>>;
+}
+
+export interface ContraCorrentePixProcessData {
+  /**
+   * Detalhes do pedido atual
+   */
+  order: Nullable<PixOrderData>;
+  /**
+   * Indica se os dados do pedido ainda estão sendo capturados
+   */
+  isLoading: boolean;
+  /**
+   * Indica se o código PIX foi copiado recentemente
+   */
+  hasCopied: boolean;
+  /**
+   * Timestamp atual
+   */
+  now: number;
+  /**
+   * Interval ID
+   */
+  nowInterval: Nullable<number>;
+  /**
+   * Indica se o navegador tem suporte ao EventSource
+   */
+  // hasEventSource: ShallowRef<Nullable<boolean>>;
+}
+
+export interface ContraCorrentePixProcessMethods {
+  /**
+   * Inicializa o `EventSource` responsável por receber as atualizações do pedido visualizado
+   */
+  pollOrder: (orderId: string) => void;
+  /**
+   * Atualiza parcialmente os dados do pedido com as informações recebidas via `EventSource`
+   */
+  patchOrder: (payload: PixOrderDataPoll) => void;
+  /**
+   * Captura os dados do pedido
+   */
+  getOrder: (orderId: string) => Promise<ResponsePattern<PixOrderData>>;
+  /**
+   * Permite ao usuário copiar o QRCode
+   */
+  handleCopyQRCode: () => Promise<void>;
+  /**
+   * Interrompe countdown responsável pela atualização do timer na tela
+   */
+  clearInterval: () => void;
+  /**
+   * Renderiza o QRImage na tela para que o usuário possa realizar a leitura
+   */
+  setQRImage: () => void;
+}
+
+export interface ContraCorrentePixOrderComputedDefinition {
+  /**
+   * Retorna o preço do pedido formatado em BRL
+   */
+  orderPrice: () => string;
+  /**
+   * Retornará o tempo restante para efetivação do pagamento
+   */
+  timmer: () => string;
+  /**
+   * Informará se o pedido já se encontra pago
+   */
+  hasPaid: () => boolean;
+  /**
+   * Informará se o pedido já se encontra expirado
+   */
+  isExpired: () => boolean;
+  /**
+   * Retorna o código QR para esse pedido
+   */
+  getQRCode: () => string;
+}
+
+export type ContraCorrentePixOrderComputed = ComputedReturnValues<ContraCorrentePixOrderComputedDefinition>;
+
+export interface ContraCorrentePixProcessWatch {
+  /**
+   * Assiste as modificações no timmer aguardando pelo momento que seja `00:00:00`
+   */
+  timmer: (time: string) => void;
+}
+
+export type ContraCorrentePixProcessContext = ContraCorrentePixProcessSetup & ContraCorrentePixProcessData & ContraCorrentePixProcessMethods & ContraCorrentePixOrderComputed;
+
+export interface PixOrderData {
+  /**
+   * Timestamp: indica em que momento esse QR code deixará de ser válido
+   */
+  due_time: number;
+  /**
+   * Valor total deste pedido
+   */
+  total: number;
+  /**
+   * Indica se o pedido está ou não pago
+   */
+  pago: boolean;
+  /**
+   * URL do QRImage escaneável
+   */
+  qrcode: string;
+  /**
+   * Linha copiável do QRCode de pagamento
+   */
+  qrcode_text: string;
+  /**
+   * Indica se o período de pagamento deste pedido passou
+   */
+  expired: boolean;
+}
+
+/**
+ * Dados retornados pelo EventSource aberto com o backend
+ */
+export type PixOrderDataPoll = Pick<PixOrderData, 'pago' | 'expired' | 'total'>;
